ed8f798af833c8be401fb002ebbd3ec2b986acab
MJC <mjc@mjczone.com>
2019-01-29 13:27:11 -0600

    experiment with app. domains

--- src/WebApp/Actions/ApiActions.cs
+++ src/WebApp/Actions/ApiActions.cs
@@ -1,32 +1,32 @@
-using System;
-using ExtCore.Infrastructure.Actions;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Rewrite;
+// using System;
+// using ExtCore.Infrastructure.Actions;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.AspNetCore.Rewrite;
 
-namespace WebApp.Actions
-{
-    public class ApiActions : IConfigureAction
-    {
-        // make sure this runs BEFORE the std aspnet actions
-        // otherwise there's a risk that static files get rendered
-        public int Priority => Priorities.ApiActions;
+// namespace WebApp.Actions
+// {
+//     public class ApiActions : IConfigureAction
+//     {
+//         // make sure this runs BEFORE the std aspnet actions
+//         // otherwise there's a risk that static files get rendered
+//         public int Priority => Priorities.ApiActions;
 
-        public void Execute(IApplicationBuilder app, IServiceProvider serviceProvider)
-        {
-            app.UseRewriter(new RewriteOptions()
-                .Add(RewriteApiRequests));
-        }
+//         public void Execute(IApplicationBuilder app, IServiceProvider serviceProvider)
+//         {
+//             app.UseRewriter(new RewriteOptions()
+//                 .Add(RewriteApiRequests));
+//         }
         
-        public static void RewriteApiRequests(RewriteContext context)
-        {
-            var request = context.HttpContext.Request;
-            var path = request.Path.Value ?? "";
+//         public static void RewriteApiRequests(RewriteContext context)
+//         {
+//             var request = context.HttpContext.Request;
+//             var path = request.Path.Value ?? "";
 
-            if(request.Host.Host.StartsWith("api.") && 
-                !path.StartsWith("/api"))
-            {
-                request.Path = "/api/" + path.TrimStart('/');
-            }
-        }
-    }
-}
+//             if(request.Host.Host.StartsWith("api.") && 
+//                 !path.StartsWith("/api"))
+//             {
+//                 request.Path = "/api/" + path.TrimStart('/');
+//             }
+//         }
+//     }
+// }
--- src/WebApp/Actions/AspNetStartupActions.cs
+++ src/WebApp/Actions/AspNetStartupActions.cs
@@ -1,9 +1,11 @@
 using System;
+using System.IO;
 using ExtCore.Infrastructure.Actions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace WebApp.Actions
 {
@@ -15,8 +17,7 @@ namespace WebApp.Actions
         {        
             services.Configure<ForwardedHeadersOptions>(options =>
             {
-                options.ForwardedHeaders =
-                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
             });
 
             services.AddCors(options => options.AddPolicy("CorsPolicy", builder => { builder.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin(); }));
@@ -41,6 +42,22 @@ namespace WebApp.Actions
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            
+            var appContentDir = Path.Combine(env.ContentRootPath, "wwwroot", "app");
+            Directory.CreateDirectory(appContentDir);
+            if (!File.Exists(Path.Combine(appContentDir, "index.html"))) {
+                File.WriteAllText(Path.Combine(appContentDir, "index.html"), @"
+<html><body>Welcome to app</body></html>                
+                ");
+            }
+
+            app.MapWhen(ctx => ctx.Request.Host.Host.StartsWith("app."), builder => {                
+                builder.UseStaticFiles(new StaticFileOptions
+                {
+                    FileProvider = new PhysicalFileProvider(appContentDir),
+                    RequestPath = "/app"
+                });
+            });
         }
     }
 }
